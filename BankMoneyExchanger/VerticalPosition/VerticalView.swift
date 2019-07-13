1c002cf1fac9d8fc4f0a699e349b576d4774e1e8
Shynkarenko Volodymyr <shynkarenko@gmail.com>
2019-07-13 14:21:55 +0300

    corrected CalendarPopup (todo - use new data for receiving http request)

--- BankMoneyExchanger/VerticalPosition/VerticalView.swift
+++ BankMoneyExchanger/VerticalPosition/VerticalView.swift
@@ -10,12 +10,46 @@ import UIKit
 
 class VerticalView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: -
+    // MARK: Properties
+    
+    @IBOutlet var CurrencyPBTable: UITableView?
+    @IBOutlet var CurrencyNBUTable: UITableView?
+    @IBOutlet var calendar: UIDatePicker?
+    
+    public var showCalendar: ( (VerticalView) -> () ) = {view in
+        guard view.calendar != nil else {
+            return
+        }
+        view.calendar?.isHidden = false
+        view.calendar?.backgroundColor = .white
     }
-    */
+    
+    public var hideCalendar: ( (VerticalView) -> () ) = {view in
+        guard view.calendar != nil else {
+            return
+        }
+        view.calendar?.isHidden = true
+    }
+    
+    // MARK: -
+    // MARK: Methods
+    
+    @IBAction func choosenData(_ sender: Any) {
+        hideCalendar(self)
+        if let date = self.calendar?.date {
+            print(date)
+        }
+    }
+    
+    @IBAction func otherCalendarButton(_ sender: Any) {
+        showCalendar(self)
+    }
+    
+    @IBAction func oneCalendarButton(_ sender: Any) {
+        showCalendar(self)
+    }
+    
+    
 
 }
