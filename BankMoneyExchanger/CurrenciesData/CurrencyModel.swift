26c921cedcfbe4a30a24675cca54c4a478876845
Shynkarenko Volodymyr <shynkarenko@gmail.com>
2019-07-13 11:39:22 +0300

    Revert "Corrected CurrencyModel, as result - modified VerticalViewController (some logic moved to model)"
    This reverts commit f067c0ea0ff619dcae3460cbe35986284c0fc081.

--- BankMoneyExchanger/CurrenciesData/CurrencyModel.swift
+++ BankMoneyExchanger/CurrenciesData/CurrencyModel.swift
@@ -10,25 +10,11 @@ import Foundation
 import SwiftyJSON
 
 class CurrencyModel {
-    struct CellPB {
-        public var currency: String
-        public var buying: String
-        public var selling: String
-        public var jumpTo: Int?
-    }
-    struct CellNBU {
-        public var currencyName: String
-        public var currency: String
-        public var value: String
-        public var count: String
-        public var jumpTo: Int?
-    }
+    
     // MARK: -
     // MARK: Properties
     
     public var data: CurrenciesData? = nil
-    public var dataPBCells: [CellPB] = []
-    public var dataNBUCells: [CellNBU] = []
     public var nameCurrency: [CurrencyName] = []
     private let pathCurrency = "CurrensyRawJSON"
     private let pathCurrencyNames = "CurrencyNamesRuJSON"
@@ -53,7 +39,6 @@ class CurrencyModel {
             print("error in decoding JSON")
         }
     //print(nameCurrency)
-        prepareCells()
     }
     
     // MARK: -
@@ -66,68 +51,5 @@ class CurrencyModel {
         }
         let expandedPath = URL(fileURLWithPath: path)
         return try! String(contentsOf: expandedPath)
-    }
-    
-    private func preparePBcells() {
-        
-        if self.data == nil {
-            print("there is no data in preparePBcells()!!!")
-            return
-        }
-        let filteredData = self.data?.exchangeRate.filter{ currency in
-            return (currency.purchaseRate != nil) && (currency.currency != nil)
-        }
-        if filteredData == nil {
-            print("there is no filteredData in  preparePBcells()")
-            return
-        }
-        filteredData?.forEach{ currency in
-            let cell: CellPB = CellPB(currency: currency.currency ?? "!!!",
-                    buying: "\(currency.purchaseRate ?? 0)",
-                    selling: "\(currency.saleRate ?? 0)",
-                    jumpTo: nil)
-            dataPBCells.append(cell)
-        }
-    }
-    
-    private func prepareNBUCells() {
-        
-        if self.data == nil {
-            print("there is no data in preparePBcells()!!!")
-            return
-        }
-        let filteredData = self.data?.exchangeRate.filter{ currency in
-            return (currency.currency != nil)
-        } // todo - simplify
-        if filteredData == nil {
-            print("there is no filteredData in  prepareNBUcells()")
-            return
-        }
-        filteredData?.forEach{ currency in
-            let curAtr = currency.currency ?? " "
-            let currencyName: String = self.nameCurrency.filter{ curr in
-                return curr.attr == curAtr
-            }.first?.name ?? "Unknown currency"
-            let cell: CellNBU = CellNBU(currencyName: currencyName,
-                            currency: curAtr,
-                            value: "\(currency.purchaseRateNB)",
-                            count: "1 UAH",
-                            jumpTo: nil)
-            dataNBUCells.append(cell)
-        }
-    }
-    
-    private func prepareCells() {
-        preparePBcells()
-        prepareNBUCells()
-        dataPBCells.enumerated().forEach{ (n, data) in
-            let indexCellNBU = dataNBUCells.firstIndex{ dataNBU in
-                return dataNBU.currency == data.currency
-            }
-            self.dataPBCells[n].jumpTo = indexCellNBU
-            if indexCellNBU != nil {
-                self.dataNBUCells[indexCellNBU!].jumpTo = n
-            }
-        }
     }
 }
