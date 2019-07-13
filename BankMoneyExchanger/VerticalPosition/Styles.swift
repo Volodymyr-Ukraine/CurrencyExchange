987dda41b63362f65af6f3ff1575d1ff9030f0ec
Shynkarenko Volodymyr <shynkarenko@gmail.com>
2019-07-13 11:52:27 +0300

    added Styles - set of color constants

---
+++ BankMoneyExchanger/VerticalPosition/Styles.swift
@@ -0,0 +1,13 @@
+//
+//  Styles.swift
+//  BankMoneyExchanger
+//
+//  Created by Vladimir on 7/13/19.
+//  Copyright © 2019 Volodymyr. All rights reserved.
+//
+
+import UIKit
+
+public let colorWhiteCell = UIColor.white
+public let colorGreenCell = UIColor.init(red: 0.5, green: 0.7, blue: 0.5, alpha: 0.15)
+public let colorSelectedCell = UIColor.lightGray
